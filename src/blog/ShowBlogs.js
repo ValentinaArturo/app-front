4b9e9101c4a0862ca2ff53d8aae635c91a878deb
ValentinaArturo <74568726+ValentinaArturo@users.noreply.github.com>
2023-09-23 00:15:10 -0600

    editar url

--- src/blog/ShowBlogs.js
+++ src/blog/ShowBlogs.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {useState, useEffect} from 'react'
 import {Link} from 'react-router-dom'
 
-const URI = 'http://localhost:8000/blogs/'
+const URI = 'http://18.117.78.96:8000/blogs/'
 
 
 const CompShowBlogs = () => {
